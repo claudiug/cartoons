116724d3dbb10ebca9dffb8d636b990804c3d57e
claudiu <claudiu.garba@gmail.com>
2013-10-24 00:48:08 +0200

    add views and methods

--- app/controllers/admin/characters_controller.rb
+++ app/controllers/admin/characters_controller.rb
@@ -1,2 +1,30 @@
 class Admin::CharactersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
