7e2c65df24970150042201b957ec166d48741c67
claudiu <claudiu.garba@gmail.com>
2013-10-30 22:59:57 +0100

    fix form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
 
   belongs_to :commentable, polymorphic: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :content, presence: true, length: {within: 4..300}
 end
