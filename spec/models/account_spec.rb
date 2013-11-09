1910c283f2758a50e227a13e099f1c99bf43cb09
claudiug <claudiu.garba@gmail.com>
2013-11-09 02:31:14 +0100

    # Include Factory Girl syntax to simplify calls to factories config.include FactoryGirl::Syntax::Methods

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,20 +3,18 @@ require 'spec_helper'
 describe "Account" do
 
   it 'is valid with email and username' do
-    account = Account.new(username:"claudiu", email:"claudiu@gmail.com", password:"qwe123",
-                          password_confirmation: "qwe123")
-    expect(account).to be_valid
+
+    expect(build(:account)).to be_valid
   end
   it 'is invalid without an email' do
-    expect(Account.new(email: nil)).to have(2).errors_on(:email)
+    expect(build(:account, email:nil)).to have(2).errors_on(:email)
 end
   it 'is invalid without an username'
   it 'is invalid with a duplicate email'
   it  'is invalid with a non email address format'
 
   it 'returns a username - email ' do
-    account = Account.new(username:"claudiu", email:"claudiu@gmail.com", password:"qwe123",
-                          password_confirmation: "qwe123")
+    account = create(:account, username:"claudiu", email:"claudiu@gmail.com")
     expect(account.name).to eq 'claudiu claudiu@gmail.com'
   end
 end
