71035bee36363eaa50c53189a2a52f094b7d4928
claudiug <claudiu.garba@gmail.com>
2013-11-09 18:39:13 +0100

    stub for controllers cartoons

--- spec/controllers/cartoons_controller_spec.rb
+++ spec/controllers/cartoons_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
-descibe CartoonsController do
+describe CartoonsController do
 
   describe "GET index" do
     context "with params q" do
-
+      it 'populate an array with that given letter'
+      it 'renders the index views'
     end
 
     context 'without params q' do
-
+      it 'populate an array with cartoons'
+      it 'render the index views'
     end
 
   end
